c75986056150cd4274bc36843c45c24837c9cc2d
fdevinar <fc_devinar@hotmail.com>
2020-05-19 17:50:06 +0530

    added pug templating

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
+// REQUIREMENTS
 const express = require('express');
 const app = express();
 
+// CONFIG
+app.set('views','./views');
+app.set('view engine','pug');
+
+// WORKOUT OBJECTS
+let exercise = {
+    name: 'Deadlift',
+    reps: 8,
+    sets: 5,
+    weight: 80,
+    muscle: 'Lower body'
+};
+
+// ROUTES
 app.get('/',(req, res) => {
-    res.send('Home');
+    res.render('index',{exercise:exercise});
 });
 
+
+// SERVER START
 app.listen(3000,() => {
-    console.log('Server initiated');
+    console.log('*** Iron Tracker server running ***');
 });
 
 
